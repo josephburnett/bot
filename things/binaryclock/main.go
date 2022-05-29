dd46ac3cd78cdf0e41f03375f80808c83bc0bd1a
Joseph Burnett <josephburnett79@gmail.com>
2022-05-29 20:46:15 +0200

    Combined display of millis, seconds, minutes and hours.

--- things/binaryclock/main.go
+++ things/binaryclock/main.go
@@ -23,11 +23,15 @@ const (
 )
 
 var (
-	colorOff     = color.RGBA{R: 0x00, G: 0x00, B: 0x00}
-	colorMillis  = color.RGBA{R: 0x01, G: 0x00, B: 0x00}
-	colorSeconds = color.RGBA{R: 0x01, G: 0x01, B: 0x00}
-	colorMinutes = color.RGBA{R: 0x00, G: 0x01, B: 0x00}
-	colorHours   = color.RGBA{R: 0x00, G: 0x00, B: 0x01}
+	colorOff         = color.RGBA{R: 0x00, G: 0x00, B: 0x00}
+	colorMillisHigh  = color.RGBA{R: 0x50, G: 0x00, B: 0x00}
+	colorMillisLow   = color.RGBA{R: 0x01, G: 0x00, B: 0x00}
+	colorSecondsHigh = color.RGBA{R: 0x50, G: 0x50, B: 0x00}
+	colorSecondsLow  = color.RGBA{R: 0x01, G: 0x01, B: 0x00}
+	colorMinutesHigh = color.RGBA{R: 0x00, G: 0x50, B: 0x00}
+	colorMinutesLow  = color.RGBA{R: 0x00, G: 0x01, B: 0x00}
+	colorHoursHigh   = color.RGBA{R: 0x00, G: 0x00, B: 0x50}
+	colorHoursLow    = color.RGBA{R: 0x00, G: 0x00, B: 0x01}
 )
 
 type clock struct {
@@ -62,17 +66,22 @@ func (c *clock) displayTime() {
 		lights[i] = colorOff
 	}
 
-	set := func(number, offset int, lightsRange [2]int, color color.RGBA) {
+	set := func(number, ringOffset int, bitRange [2]int, high, low color.RGBA) {
 		for i := range lights {
-			if i < lightsRange[0] || i >= lightsRange[1] {
+			if i < bitRange[0] || i >= bitRange[1] {
+				// Display a subset of the bits.
 				continue
 			}
+			// Read up to 10 bits from the number.
 			mask := 1 << i
-			index := (i + offset) % len(lights)
+			// Display the bits at offset along ring.
+			index := (i + ringOffset) % len(lights)
 			if on := number & mask; on > 0 {
-				lights[index] = color
+				// Bit is a high (1).
+				lights[index] = high
 			} else {
-				lights[index] = colorOff
+				// Bit is a low (0).
+				lights[index] = low
 			}
 		}
 	}
@@ -80,10 +89,20 @@ func (c *clock) displayTime() {
 	t := time.Now()
 	switch c.display {
 	case displayMillisAndSeconds:
-		millis := t.Nanosecond() / 1000 / 1000
-		seconds := t.Second()
-		set(millis, 0, [2]int{6, 10}, colorMillis)
-		set(seconds, 0, [2]int{0, 5}, colorSeconds)
+		millis := t.Nanosecond()/1000/1000 + 1
+		seconds := t.Second() + 1
+		set(millis, 0, [2]int{6, 10}, colorMillisHigh, colorMillisLow)
+		set(seconds, 0, [2]int{0, 6}, colorSecondsHigh, colorSecondsLow)
+	case displaySecondsAndMinutes:
+		seconds := t.Second() + 1
+		minutes := t.Minute() + 1
+		set(seconds, 4, [2]int{2, 6}, colorSecondsHigh, colorSecondsLow)
+		set(minutes, 0, [2]int{0, 6}, colorMinutesHigh, colorMinutesLow)
+	case displayMinutesAndHours:
+		minutes := t.Minute() + 1
+		hours := t.Hour()%12 + 1
+		set(minutes, 4, [2]int{0, 6}, colorMinutesHigh, colorMinutesLow)
+		set(hours, 0, [2]int{0, 4}, colorHoursHigh, colorHoursLow)
 	}
 	c.board.SetLights(lights)
 }
